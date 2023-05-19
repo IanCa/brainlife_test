d63ba09c93cfd9f3babf2e5ad74de5010742afe0
IanCa <ianrcallanan@gmail.com>
2023-05-19 13:48:52 -0500

    testing

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 
 import sys
 import nibabel as nib
+import pandas
 
 #just dump input image header to output.txt
+print("HI1")
+print(list(sys.argv))
 img=nib.load(sys.argv[1])
 f=open("output.txt", "w")
+print("HI!")
 f.write(str(img.header))
 f.close()
